0d5fc65e307e3aa7d607a2866e6b3fcdc21794a2
christine927t <christine927t@gmail.com>
2021-02-05 17:41:56 -0500

    successfully fetching data from API

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
-import withContext from './Context';
+// import withContext from './Context';
 
-let listCourses = withContext(Component)
-console.log(listCourses)
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      courses: []
+    };
+  }
 
-function App() {
-      // attempting api call here instead of Data.js
-    listCourses = async () =>{
-        const response = await this.data.getCourses();
-        console.log(response)
-        if (response.status === 200){
-            console.log('Successfully getting courses!')
-            return response.json().then(data => data)
-        }
-        else if (response.status === 401) {
-          console.log('Not getting courses!')
-          return null;
-        }
-        else {
-          console.log('Not getting courses!')
-          throw new Error();
-        }
-    }
-  // console.log(this.data.getCourses())
+  async componentDidMount(){
+    const response = await fetch('http://localhost:5000/api/courses')
+    const json = await response.json();
+    this.setState({ courses: json })  
+    // .then(response => response.json())
+      // .then(responseData => {
+      //   this.setState({ courses: responseData.data });
+      //   console.log(responseData.data)
+      // })
+      // .catch(error => {
+      //   console.log('Error fetching and parsing data', error)
+      // })
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          <ul>
-              <li>
-                {listCourses.response}
-              </li>
-            </ul>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-  )
+  render(){
+    console.log(this.state.courses);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+            <ul>
+                <li>
+                </li>
+              </ul>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+    )
+  }
 }
-
-export default App;
--- client/src/Data.js
+++ client/src/Data.js
@@ -1,7 +1,7 @@
 import config from './config';
 
 export default class Data {
-    api(path, method, body = null) {
+    api(path, method, body = null, requiresAuth = false, credentials = null) {
         const url = config.apiBaseUrl + path;
 
         const options = {
@@ -15,11 +15,18 @@ export default class Data {
             options.body = JSON.stringify(body);
         }
 
+        if(body !== null) {
+            if(requiresAuth){
+                const encodedCredentials = btoa(`${credentials.username}:${credentials.password}`);
+                options.headers['Authorization'] = `Basic ${encodedCredentials}`;
+            }
+        }
+
         return fetch(url, options)
     }
 
-    async getCourses(){
-        const response = await this.api('/courses', 'GET', null, true);
+    async getCourses(username, password){
+        const response = await this.api('/courses', 'GET', null, true, { username, password });
         if (response.status === 200){
             console.log('Successfully getting courses!')
             return response.json().then(data => data)
